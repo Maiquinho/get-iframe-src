0a5d1bd283cd99ad6d9813cfc24d787f56a8a2e4
Michael Matheus <michaelmatheus2001@gmail.com>
2023-04-26 15:35:19 -0300

    feat: app

---
+++ js/index.js
@@ -0,0 +1,78 @@
+/**
+ * 
+ * The input element to execute RegEx code 
+ * 
+*/
+
+const inputDataValue = document.querySelector('[data-action="get-iframe-src"]');
+
+
+/**
+ * 
+ * The main function
+ * 
+*/
+
+function getIframeSrc(string) {
+    return string.replace(/<iframe[^>]*src=[\"|']([^'\"]+)[\"|'][^>]*><\/iframe>/i, "\$1");
+}
+
+
+/**
+ * 
+ * The copy text function
+ * 
+*/
+
+function copyInputValue() {
+    const inputValue = inputDataValue.value;
+    const message = document.querySelector('#msg');
+
+
+    if (inputValue != '' && inputValue.includes('http')) {
+
+        navigator.clipboard
+            .writeText(inputValue)
+            .then(() => {
+
+                setTimeout(() => {
+        
+                    message.innerHTML = 'Text copied';            
+                    setTimeout(() => {
+                        message.innerHTML = '';
+                    }, 1500)
+
+                }, 100)
+
+            })
+            .catch(() => {
+                alert("Something is wrong, try again later!");
+            });
+            
+    } else {
+
+        setTimeout(() => {
+
+            message.innerHTML = 'Error, invalid content!';
+
+            setTimeout(() => {
+                message.innerHTML = '';
+            }, 1500)
+
+        }, 100);
+
+    }
+
+}
+
+
+/**
+ * 
+ * The input element eventListener executing the RegEx function
+ * 
+*/
+
+inputDataValue.addEventListener('input', (e) => {
+    e.target.value = getIframeSrc(e.target.value);
+    copyInputValue();
+});
